9982da52a4ca295479c56383b493c9f0d82f1330
AhmedNadar <ahmedfay@hotmail.com>
2013-08-30 07:17:03 -0400

    text update

--- walmart.rb
+++ walmart.rb
@@ -7,17 +7,6 @@ File.open("walmart.html", 'w') do |f|
 	url = "http://www.walmart.ca/canada-estore/search/searchcontainer.jsp?&addFacet=SRCH%3Aapple&trail=&searchString=apple&pageNo=0&startPageNo=1&resultsInPage=30&sortMode=&sortProperty=&sortOrder=/canada-estore/search/searchcontainer.jsp?&addFacet=SRCH%3Aapple&trail=&searchString=apple&pageNo=0&startPageNo=1&resultsInPage=30&sortMode=&sortProperty=&sortOrder=&startSearch=yes&compare=&_requestid=45012"
 	doc = Nokogiri::HTML(open(url))
 	page_title = doc.at_css("title").text
-	# doc.css(".hproduct").each do |item|
-		# 	product_name = item.at_css("#view-product-details a").text
-		# 	product_link = item.at_css("#view-product-details a")[:href]
-		# 	price = item.at_css(".original-price").text[/\$[0-9\.]+/]
-		# 	# #print name and price in terminal
-		# 	# puts "#{product_name} -  #{price}"
-		# 	# puts "#{product_link}" 
-		# 	f.puts("\t<li class=\"name\">#{product_name}</li>")
-		# 	f.puts("\t<span class=\"price\">#{price}</span>")
-		# 	f.puts("\t<a class=\"link\" href=\"#{product_link}\">more details</a>")
-	# end
 f.puts <<-eos
 
 	<!DOCTYPE html>
@@ -59,45 +48,40 @@ f.puts <<-eos
 	      <!-- Jumbotron -->
 	      <div class="jumbotron">
 	        <h1>Fun with Nokogiri and Bootstrap</h1>
-	        <p class="lead">Fetish for Apple products from Walmart.com using Nocogiri and open-uri.</p>
+	        <p class="lead">Fetch for Apple products from Walmart.com using Nocogiri</p>
 	        <p><a class="btn btn-lg btn-success" href="https://github.com/AhmedNadar/store_list_by_nokogiri">Get the repo</a></p>
 	      </div>
-	eos
+eos
 	doc.css(".hproduct").each do |item|
 		product_name = item.at_css("#view-product-details a").text
 		product_link = item.at_css("#view-product-details a")[:href]
 		product_image = item.at_css("#view-product-image img")[:src]
 		product_image_alt = item.at_css("#view-product-image img")[:alt]
 		price = item.at_css(".original-price").text[/\$[0-9\.]+/]
-		# #print name and price in terminal
-		# puts "#{product_name} -  #{price}"
-		# puts "#{product_link}" 
-		#f.puts("\t<li class=\"name\">#{product_name}</li>")
-		#f.puts("\t<span class=\"price\">#{price}</span>")
-		#f.puts("\t<a class=\"link\" href=\"#{product_link}\">more details</a>")
-
-
 		f.puts("<div class=\"row\">")
 			f.puts("<div class=\"col-lg-3 box3_extra_style product_bottom_border\">")
 				f.puts("<h4>#{product_name}</h4>")
 			   f.puts("<img class=\"img\" src=\"#{product_image}\" alt=\"#{product_image_alt}\">")
+			   f.puts("<p>#{price}</p>")
 	   		f.puts("<p><a class=\"btn btn-primary\" href=\"#{product_link}\">View details &raquo;</a></p>")
 	      f.puts("</div>")
 	       
 			f.puts("<div class=\"col-lg-3 box3_extra_style product_bottom_border\">")
 				f.puts("<h4>#{product_name}</h4>")
 			   f.puts("<img class=\"img\" src=\"#{product_image}\" alt=\"#{product_image_alt}\">")
-	   		f.puts("<p><a class=\"btn btn-primary\" href=\"#{product_link}\">View details &raquo;</a></p>")
+			   f.puts("<p>#{price}</p>")
+			   f.puts("<p><a class=\"btn btn-primary\" href=\"#{product_link}\">View details &raquo;</a></p>")
 	      f.puts("</div>")
 
 			f.puts("<div class=\"col-lg-3 box3_extra_style product_bottom_border\">")
 				f.puts("<h4>#{product_name}</h4>")
 			   f.puts("<img class=\"img\" src=\"#{product_image}\" alt=\"#{product_image_alt}\">")
+			   f.puts("<p class=\"price\">#{price}</p>")
 	   		f.puts("<p><a class=\"btn btn-primary\" href=\"#{product_link}\">View details &raquo;</a></p>")
 	      f.puts("</div>")
 	      f.puts("</div>")
 	end
-	f.puts <<-eos
+f.puts <<-eos
 	      <!-- Site footer -->
 	      <div class="footer">
 	        <p>&copy; Ahmed Nadar 2013</p>
@@ -108,6 +92,6 @@ f.puts <<-eos
 	    <!-- Placed at the end of the document so the pages load faster -->
 	  </body>
 	</html>
-	eos
+eos
 end
 
